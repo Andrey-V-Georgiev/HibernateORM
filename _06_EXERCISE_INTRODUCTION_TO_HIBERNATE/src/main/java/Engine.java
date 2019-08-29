b60c9f89b99055f335592f79534fac1b73355e28
Andrey_V_Georgiev <andrey.v.georgiev@gmail.com>
2019-08-29 13:11:16 +0300

    added 2,4,5 exercises from _06_EXERCISE_INTRODUCTION_TO_HIBERNATE

--- _06_EXERCISE_INTRODUCTION_TO_HIBERNATE/src/main/java/Engine.java
+++ _06_EXERCISE_INTRODUCTION_TO_HIBERNATE/src/main/java/Engine.java
@@ -3,53 +3,46 @@ import entities.Employee;
 import entities.Town;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 import java.util.Scanner;
 
 class Engine implements Runnable {
-    private EntityManager entityManager;
+    private EntityManager em;
 
     Engine(EntityManager entityManager) {
-        this.entityManager = entityManager;
+        this.em = entityManager;
     }
 
     @Override
     public void run() {
-        //this.containsEmployee();
-        addAddressAndUpdateEmployee();
+        //this._02_RemoveObjects();
+        //this._03_ContainsEmployee();
+        //this._04_EmployeesWithSalaryOver50000();
+        //this._05_EmployeesFromDepartment();
+        //this._06_AddAddressAndUpdateEmployee();
+
     }
 
-    private void addAddressAndUpdateEmployee() {
-        Scanner scanner = new Scanner(System.in);
-        String lastName = scanner.nextLine();
-
-        this.entityManager.getTransaction().begin();
-        Town town = this.entityManager
-                .createQuery("FROM Town WHERE id=32", Town.class)
-                .getSingleResult();
-        Address address = new Address();
-        address.setText("Vitoshka 17");
-        address.setTown(town);
-        this.entityManager.persist(address);
-        this.entityManager.getTransaction().commit();
-
-        this.entityManager.getTransaction().begin();
-        Employee employee = this.entityManager
-                .createQuery("FROM Employee WHERE lastName=:lastName", Employee.class)
-                .setParameter("lastName", lastName)
-                .getSingleResult();
-        employee.setAddress(address);
-        this.entityManager.flush();
-        this.entityManager.getTransaction().commit();
+    private void _02_RemoveObjects() {
+        this.em.getTransaction().begin();
+        List<Town> towns = this.em
+                .createQuery("FROM Town t WHERE LENGTH(t.name) > 5", Town.class)
+                .getResultList();
+        towns.forEach(t -> this.em.detach(t));
+        towns.forEach(t -> {
+            t.setName(t.getName().toLowerCase());
+            this.em.merge(t);
+        });
+        this.em.getTransaction().commit();
     }
 
-
-    private void containsEmployee() {
+    private void _03_ContainsEmployee() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter employee name: ");
         String fullName = scanner.nextLine();
-        this.entityManager.getTransaction().begin();
+        this.em.getTransaction().begin();
         try {
-            Employee employee = this.entityManager
+            Employee employee = this.em
                     .createQuery("FROM Employee WHERE CONCAT(firstName,' ',lastName)=:fullName",
                             Employee.class)
                     .setParameter("fullName", fullName)
@@ -58,7 +51,59 @@ class Engine implements Runnable {
         } catch (Exception e) {
             System.out.println("No");
         }
-        this.entityManager.getTransaction().commit();
+        this.em.getTransaction().commit();
     }
+
+    private void _04_EmployeesWithSalaryOver50000() {
+        this.em.getTransaction().begin();
+        List<String> employeesFirstNames = this.em
+                .createQuery("SELECT e.firstName FROM Employee e WHERE e.salary > 50000", String.class)
+                .getResultList();
+        employeesFirstNames.forEach(System.out::println);
+        this.em.getTransaction().commit();
+    }
+
+    private void _05_EmployeesFromDepartment() {
+        this.em.getTransaction().begin();
+        List<Employee> employees = this.em
+                .createQuery("FROM Employee AS e " +
+                                 "WHERE e.department.name ='Research and Development' " +
+                                 "ORDER BY e.salary, e.id",
+                        Employee.class)
+                .getResultList();
+        employees.forEach(e -> System.out.printf("%s %s from %s - $%.2f\n",
+                e.getFirstName(), e.getLastName(), e.getDepartment().getName(), e.getSalary()));
+        this.em.getTransaction().commit();
+    }
+
+    private void _06_AddAddressAndUpdateEmployee() {
+        Scanner scanner = new Scanner(System.in);
+        String lastName = scanner.nextLine();
+
+        this.em.getTransaction().begin();
+        Town town = this.em
+                .createQuery("FROM Town WHERE id=32", Town.class)
+                .getSingleResult();
+        Address address = new Address();
+        address.setText("Vitoshka 16");
+        address.setTown(town);
+        this.em.persist(address);
+        this.em.getTransaction().commit();
+
+        this.em.getTransaction().begin();
+        try {
+            Employee employee = this.em
+                    .createQuery("FROM Employee WHERE lastName=:lastName", Employee.class)
+                    .setParameter("lastName", lastName)
+                    .getSingleResult();
+            employee.setAddress(address);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        this.em.getTransaction().commit();
+
+    }
+
 }
 
