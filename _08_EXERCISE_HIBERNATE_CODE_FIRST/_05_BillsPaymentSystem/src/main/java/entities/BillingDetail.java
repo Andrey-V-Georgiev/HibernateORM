bf05db856c03041849f23f46aedac3de738fe4c0
Andrey_V_Georgiev <andrey.v.georgiev@gmail.com>
2019-09-08 16:51:15 +0300

    added _05_BillsPaymentSystem

---
+++ _08_EXERCISE_HIBERNATE_CODE_FIRST/_05_BillsPaymentSystem/src/main/java/entities/BillingDetail.java
@@ -0,0 +1,33 @@
+package entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "billing_details")
+public class BillingDetail extends BaseEntity{
+
+    private String number;
+    private User owner;
+
+    public BillingDetail() {
+    }
+
+    @Column(name = "number")
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @ManyToOne()
+    @JoinColumn(name = "owner_id", referencedColumnName = "id")
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+}
